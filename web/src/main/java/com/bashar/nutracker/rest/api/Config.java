6f65abf236a7711324fb73a0e22c08f9a7b6f358
Bashar <basharlabadi@gmail.com>
2018-08-18 14:06:49 -0400

    remove CORS config

--- web/src/main/java/com/bashar/nutracker/rest/api/Config.java
+++ web/src/main/java/com/bashar/nutracker/rest/api/Config.java
@@ -1,12 +1,14 @@
 package com.bashar.nutracker.rest.api;
 
 import com.bashar.nutracker.core.CoreConfig;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -21,17 +23,31 @@ public class Config {
 @Configuration
 class WebConfig extends WebMvcConfigurerAdapter {
 
-    @Value("${app.client.allowedOrigin}")
-    String allowedClientOrigin;
+    // @Value("${app.client.allowedOrigin}")
+    // String allowedClientOrigin;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-            // by default only get, post are allowed
-            .allowedMethods("HEAD","GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedOrigins(allowedClientOrigin)
-            .maxAge(3600);
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/api/**")
+    //         // by default only get, post are allowed
+    //         .allowedMethods("HEAD","GET", "POST", "PUT", "DELETE", "OPTIONS")
+    //         .allowedOrigins(allowedClientOrigin)
+    //         .maxAge(3600);
+    // }
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+          = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("Request: ");
+        return filter;
     }
+
+
 }
 
 
